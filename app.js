b4c1b850ffc4f78b54e6cd6134c92ec7df3481ea
lanxin1216 <131834711+lanxin1216@users.noreply.github.com>
2023-11-23 22:06:27 +0800

    前端

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-// app.js
+//app.js
+var http = require("utils/http.js");
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+  onLaunch: function () {
+    console.log('shMallj.v230313')
+    // http.getToken();
+    // wx.getSetting({
+    //   success(res) {
+    //     if (!res.authSetting['scope.userInfo']) {
+    //       wx.navigateTo({
+    //         url: '/pages/login/login',
+    //       })
+    //     }
+    //   }
+    // })  
   },
   globalData: {
-    userInfo: null
+    // 定义全局请求队列
+    requestQueue: [],
+    // 是否正在进行登陆
+    isLanding: true,
+    // 购物车商品数量
+    totalCartCount: 0
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,25 +1,30 @@
 var http = require("../../utils/http.js");
-var crypto = require("../../utils/crypto.js")
-
-// pages/login/login.js
+var crypto = require("../../utils/crypto.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    //用户名
+    // 用户名
     userName: '',
-    //密码
+    // 密码
     password: '',
-    //是否显示注册
-    isRegister:false
+    // 是否显示注册
+    isRegister: false
+  },
+
+  onGotUserInfo: function (res) {
+    http.updateUserInfo();
+    wx.navigateBack({
+      delta: 1
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad: function (options) {
     this.setData({
       isRegister: options.isRegister == 0
     })
@@ -28,52 +33,52 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
+  onShareAppMessage: function () {
 
   },
-
+  
   /**
    * 输入框的值
    */
@@ -81,27 +86,27 @@ Page({
     const type = e.currentTarget.dataset.type
     if (type == 'account') {
       this.setData({
-          userName: e.detail.value
+        userName: e.detail.value
       })
     } else if (type == 'password') {
       this.setData({
-          password: e.detail.value
+        password: e.detail.value
       })
     }
   },
 
   /**
-   * 切换注册\登录
+   * 切换注册/登录
    */
   handleChangeShowType() {
-    let str = this.data.isRegister ? '1' : '0'
+    let str =  this.data.isRegister ? '1' : '0'
     wx.redirectTo({
       url: '/pages/login/login?isRegister=' + str
     })
   },
 
   /**
-   * 注册\登录按钮
+   * 注册/登录按钮
    */
   handleLoginOrRegister() {
     const that = this
@@ -114,12 +119,11 @@ Page({
     }
     if (!this.data.password.trim()) {
       wx.showToast({
-          title: '请输入密码',
-          icon: 'none'
+        title: '请输入密码',
+        icon: 'none'
       })
       return
     }
-
     const params = {
       url: this.data.isRegister ? '/user/register' : '/login',
       method: "POST",
@@ -137,13 +141,14 @@ Page({
           })
           wx.showToast({
             title: '注册成功，请登录',
-            icon: 'none',
+            icon: 'none'
           })
         }else {
           wx.switchTab({
             url: '/pages/index/index',
-          })
+          })          
         }
+
       }
     };
     http.request(params);
